fce54ac23e9ff3d1fb0b3ab8b716a28e82d0b42f
sandunrmst <sandunalugc@gmail.com>
2024-01-15 13:37:35 +0530

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-orange-400 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
