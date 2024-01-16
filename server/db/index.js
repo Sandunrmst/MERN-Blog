41b794558c711e1e14144429e85cadd623cb452c
sandunrmst <sandunalugc@gmail.com>
2024-01-16 13:57:38 +0530

    build server configurations

---
+++ server/db/index.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://sandunrmst:PeTUjvvnKxWeUv63@clusterrmst.rn0zuff.mongodb.net/"
+  )
+  .then(() => console.log("Mongo DB Connected Success!"))
+  .catch((e) => console.log(e));
