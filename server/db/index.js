3dd8f187eb78c9c93f5a151ff38df092fbffb672
Sandun Tharuka <49017841+Sandunrmst@users.noreply.github.com>
2024-01-17 16:03:53 +0530

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 mongoose
   .connect(
-    "mongodb+srv://sandunrmst:PeTUjvvnKxWeUv63@clusterrmst.rn0zuff.mongodb.net/"
+    "mongodb+srv://sandunrmst:PeTUjvvndcxKxWeUv63@clusterrmst.rn0zuff.mongodb.net/"
   )
   .then(() => console.log("Mongo DB Connected Success!"))
   .catch((e) => console.log(e));
